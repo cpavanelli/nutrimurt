0fd05c1d32870444152a1ce43832a153d290246b
Caio Pavanelli <cpavanelli@gmail.com>
2026-04-26 22:12:17 -0300

    config: spliting vite chunks, removing version from docker compose file.

--- nutrimurt.Web/src/main.tsx
+++ nutrimurt.Web/src/main.tsx
@@ -1,20 +1,21 @@
-import { StrictMode } from 'react'
+import { StrictMode, lazy, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { ptBR } from '@clerk/localizations';
 import App from './App';
-import PatientsPage from './features/patients/PatientsPage';
-import QuestionariesPage from './features/questionaries/QuestionarriePage';
-import AnswerPage from './features/answers/AnswerPage';
-import ViewAnswerPage from './features/answers/ViewAnswerPage';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import PatientSummary from './features/patients/PatientSummary';
 import ProtectedRoute from './components/ProtectedRoute';
 import SignInPage from './pages/SignInPage';
 import Layout from './components/Layout';
+
+const PatientsPage = lazy(() => import('./features/patients/PatientsPage'));
+const QuestionariesPage = lazy(() => import('./features/questionaries/QuestionarriePage'));
+const AnswerPage = lazy(() => import('./features/answers/AnswerPage'));
+const ViewAnswerPage = lazy(() => import('./features/answers/ViewAnswerPage'));
+const PatientSummary = lazy(() => import('./features/patients/PatientSummary'));
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -58,9 +59,10 @@ const rootEl = document.getElementById('root')!;
 createRoot(rootEl).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} localization={ptBR}>
-      <RouterProvider router={router} />
+      <Suspense fallback={null}>
+        <RouterProvider router={router} />
+      </Suspense>
       <ToastContainer position="top-right" theme="dark" autoClose={3000} />
     </ClerkProvider>
   </StrictMode>
 );
-
